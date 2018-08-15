8cdecea95aae7846d73d6ec3938fec05d7ece0dc
kenzan8000 <kenzan8000@gmail.com>
2018-08-15 16:13:04 -0700

    update to expand columns by adding destination ids user likes

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import urllib.request
 import json
 
 
-df = pd.read_csv('./tables/user.csv', sep=',')
-
-
 ## expand columns
-#    # ip
+#    # by ip
+#df = pd.read_csv('./tables/user.csv', sep=',')
 #columns = ['Current sign in ip', 'Last sign in ip']
 #fields = [
 #    'country_code',
@@ -33,62 +31,69 @@ df = pd.read_csv('./tables/user.csv', sep=',')
 #df.to_csv('./tables/user.csv', encoding='utf-8', index=False)
 
 
-## user
-#    # user
-#columns = [
-#    'Id',
-#    'Current sign in at',
-#    'Last sign in at',
-#    'Current sign in ip',
-#    'Last sign in ip',
-#    'User agent',
-#    'Browser name',
-#    'Device name',
-#    'Os name'
-#]
-#df_user = df.query('Admin != "t"')[columns]
-#df_user = df_user.rename(columns={'Id': 'User'})
-#    # user destination offset
-#df = pd.read_csv('./tables/user_destination_offset.csv', sep=',')
-#columns = [
-#    'User',
-#    'Offset',
-#    'Round',
-#    'Created at',
-#    'Updated at',
-#    'Start',
-#    'Offset.1'
-#]
-#df_user_destination_offset = df[columns]
-#df_user_destination_offset = df_user_destination_offset.rename(columns={'Offset': 'Offset index [User destination offset]', 'Round': 'Round [User destination offset]', 'Created at': 'Created at [User destination offset]', 'Updated at': 'Updated at [User destination offset]', 'Start': 'Start [User destination offset]', 'Offset.1': 'Offset [User destination offset]'})
-#    # user destination
-#df_user_destination = pd.DataFrame({'User' : []})
-#df = pd.read_csv('./tables/user_destination.csv', sep=',')
-#columns = [
-#    'User',
-#    'Destination',
-#    'Created at',
-#    'Updated at'
-#]
-#df = df.query('Value > 0')[columns]
-#del columns[0]
-#for column in columns:
-#    sf = df.groupby(['User'])[column].apply(list)
-#    new_df = pd.DataFrame({'User': sf.index, column: sf.values})
-#    if df_user_destination.empty:
-#        df_user_destination = new_df
-#    else:
-#        df_user_destination = pd.merge(df_user_destination, new_df, on='User', how='outer')
-#df_user_destination = df_user_destination.rename(columns={'Destination': 'Destination [User destinations]', 'Created at': 'Created at [User destinations]', 'Updated at': 'Updated at [User destinations]', 'Value': 'Value [User destinations]'})
-#    # join
-#df_user = pd.merge(df_user, df_user_destination_offset, on='User', how='outer')
-#df_user = pd.merge(df_user, df_user_destination, on='User', how='outer')
-#    # save
-#df_user.to_csv('./datas/user.csv', encoding='utf-8', index=False)
-#
-#
-## destination
-#df = pd.read_csv('./tables/destination.csv', sep=',')
-#df_destination = df
-#    # save
-#df_destination.to_csv('./datas/destination.csv', encoding='utf-8', index=False)
+# destination
+df = pd.read_csv('./tables/destination.csv', sep=',')
+columns = df.columns.drop(['Subtitle', 'Desc', 'Category'])
+df_destination = df[columns]
+    # save
+df_destination.to_csv('./datas/destination.csv', encoding='utf-8', index=False)
+
+
+# user
+    # user
+df = pd.read_csv('./tables/user.csv', sep=',')
+columns = df.columns.drop(['Email', 'Reset password sent at', 'Remember created at', 'Admin', 'Line', 'Name', 'Access token', 'Refresh token'])
+df_user = df.query('Admin != "t"')[columns]
+df_user = df_user.rename(columns={'Id': 'User'})
+    # user destination offset
+df = pd.read_csv('./tables/user_destination_offset.csv', sep=',')
+columns = df.columns.drop(['Id'])
+new_names = {}
+for column in columns:
+    new_name = "%s" % column
+    if new_name.startswith('Offset.1'):
+        new_name = new_name.replace('Offset.1', 'Offset [User destination offset]')
+    elif new_name.startswith('Offset'):
+        new_name = new_name.replace('Offset', 'Offset index [User destination offset]')
+    elif new_name.startswith('User') == False:
+        new_name = "%s [User destination offset]" % column
+    new_names[column] = new_name
+df_user_destination_offset = df[columns]
+df_user_destination_offset = df_user_destination_offset.rename(columns=new_names)
+    # user destination
+df_user_destination = pd.DataFrame({'User' : []})
+df = pd.read_csv('./tables/user_destination.csv', sep=',')
+columns = df.columns.drop(['Id', 'User result', 'Value'])
+df = df.query('Value > 0')[columns]
+new_names = {}
+for column in columns:
+    if column.startswith('User'):
+        continue
+    sf = df.groupby(['User'])[column].apply(list)
+    new_df = pd.DataFrame({'User': sf.index, column: sf.values})
+    if df_user_destination.empty:
+        df_user_destination = new_df
+    else:
+        df_user_destination = pd.merge(df_user_destination, new_df, on='User', how='outer')
+    new_names[column] = "%s [User destinations]" % column
+df_user_destination = df_user_destination.rename(columns=new_names)
+        # expand columns
+        # by destination id
+lists = {}
+destination_ids = df_destination['Id'].values
+for destination_id in destination_ids:
+    lists[destination_id] = []
+destination_lists = df_user_destination['Destination [User destinations]'].values
+for destination_list in destination_lists:
+    for destination_id in destination_ids:
+        value = 0
+        if destination_id in destination_list:
+            value = 1
+        lists[destination_id].append(value)
+for destination_id in destination_ids:
+    df_user_destination["Destination %s" % destination_id] = pd.Series(lists[destination_id]).values
+    # join
+df_user = pd.merge(df_user, df_user_destination_offset, on='User', how='outer')
+df_user = pd.merge(df_user, df_user_destination, on='User', how='outer')
+    # save
+df_user.to_csv('./datas/user.csv', encoding='utf-8', index=False)
